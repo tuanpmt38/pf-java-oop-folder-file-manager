eff23242631a32c3189e07d2a680e1e0cef9f1df
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-18 21:43:14 +0700

    pf-java-oop files and forder manager

---
+++ ListFileFoder.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ListFileFoder {
+    public static void directoryTree (String path) throws IOException {
+        File dir = new File(path);
+        // kiểm tra file or folder tồn tại ko
+        if(dir.exists()){
+            System.out.println("Folder " +path+ "does not exits ");
+        }
+        //kiểm tra có phải thư mục ko
+        if(dir.isDirectory()){
+            File[] list = dir.listFiles();//listFiles lấy danh sách file hay thư mục trong thư mục
+            if(list!=null){
+                for (File file:list){
+                    System.out.println(file.getCanonicalPath());//lấy đường dẫn file hay folder
+                    directoryTree(file.getCanonicalPath());
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("The program shows the directory tree");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the path of the directory: ");
+
+        String path = scanner.nextLine();
+
+        directoryTree(path);
+    }
+}
